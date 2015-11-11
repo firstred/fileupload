7de8f6d2975862a8bf6e2bf2e700132f719f6298
Michael Dekker <git@michaeldekker.nl>
2015-11-11 16:26:38 +0100

    Comply with validation

--- translations/nl.php
+++ translations/nl.php
@@ -11,3 +11,9 @@ $_MODULE['<{fileupload}prestashop>fileupload_5058f1af8388633f609cadb75a75dc9d'] 
 $_MODULE['<{fileupload}prestashop>fileupload_d3ed7a84c7274751955098fc2d1dd2b8'] = 'Ongeldige bestandsextensies';
 $_MODULE['<{fileupload}prestashop>fileupload_130b3d918858ab70e130f882bed5edbe'] = 'Toegestane bestandsextensies';
 $_MODULE['<{fileupload}prestashop>fileupload_128ec2b116d248f5522ed49650647117'] = 'Kies de toegestande bestandsextensies. Gebruik een komma om deze te scheiden (bijv. gif,jpg,psd,zip). Laat dit veld leeg om de controle op bestandsextensies uit te schakelen.';
+$_MODULE['<{fileupload}prestashop>extra_387baf0199e7c9cc944fae94e96448fa'] = 'Overig';
+$_MODULE['<{fileupload}prestashop>extra_0584e445d656b83b431227bb80ff0c30'] = 'Doneer';
+$_MODULE['<{fileupload}prestashop>extra_77f05bf687f457d6fa20023efd04b36c'] = 'Als deze module u geholpen heeft, doe een donatie om de ontwikkeling ervan te ondersteunen.';
+$_MODULE['<{fileupload}prestashop>extra_71095c56c641f2c4a4f189b9dfcd7a38'] = 'nl_NL';
+$_MODULE['<{fileupload}prestashop>extra_bbaff12800505b22a853e8b7f4eb6a22'] = 'Contact';
+$_MODULE['<{fileupload}prestashop>extra_e347ae0189c5b9114ac2d67f55fd313e'] = 'Deze module heeft een topic op het PrestaShop-forum:';
